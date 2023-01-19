b5fbb0e51c77fcd697cfcacb4e9d145dfe7eab1c
lhazard22 <lhazard22@boyslatinmd.com>
2023-01-19 16:03:55 -0500

    Jared Is the programmer of Team 2534

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,15 +1,22 @@
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DriverStation;
+// import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 //import frc.robot.subsystems.Shooter;
 //import edu.wpi.first.wpilibj.PneumaticHub;
+import frc.robot.subsystems.DriveTrain;
 
 
 public class Robot extends TimedRobot {
+  AHRS ahrs;
+  
   private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
   //Shooter m_shooter;
@@ -19,7 +26,12 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
-    //pneumaticHub.enableCompressorAnalog(Constants.minPSI, Constants.maxPSI);
+   //  try {
+    // ahrs = new AHRS(SPI.Port.kMXP); /* Alternatives:  SPI.Port.kMXP, I2C.Port.kMXP or SerialPort.Port.kUSB */
+  // } catch (Exception ex) {
+  //  DriverStation.reportError("Error instantiating NavX kMXP" + ex.getMessage(), true);
+  //  }
+    
   }
 
 
@@ -41,7 +53,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-
+    
     //m_robotContainer.m_DriveTrain.gyro.reset();
 
     //m_autonomousCommand = m_robotContainer.getAutonomousCommand();
