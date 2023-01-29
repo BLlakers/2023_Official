9a078c79afcbb44e91d211812fe1a27bdb350e90
BLlakers <jdonnelly@boyslatinmd.com>
2023-01-29 12:10:25 -0500

    Deleting dead code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,11 +1,8 @@
 //Hey Jared
 package frc.robot;
 
-import com.kauailabs.navx.frc.AHRS;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 // import edu.wpi.first.wpilibj.DriverStation;
-// import edu.wpi.first.wpilibj.SPI;
 // import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -14,24 +11,15 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 
 public class Robot extends TimedRobot {
-  AHRS ahrs;
   
   private Command m_autonomousCommand;
   private Command m_waitcommand;
   private RobotContainer m_robotContainer;
-  //Shooter m_shooter;
-  //PneumaticHub pneumaticHub = new PneumaticHub(20);
   
   
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
-   //  try {
-    // ahrs = new AHRS(SPI.Port.kMXP); /* Alternatives:  SPI.Port.kMXP, I2C.Port.kMXP or SerialPort.Port.kUSB */
-  // } catch (Exception ex) {
-  //  DriverStation.reportError("Error instantiating NavX kMXP" + ex.getMessage(), true);
-  //  }
-    
   }
 
 
