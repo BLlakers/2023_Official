7713ad7582a7957a9c3c206fb8b334b900c57ee0
BLlakers <jdonnelly@boyslatinmd.com>
2023-02-02 18:34:27 -0500

    2/1/23 Ben

--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -6,12 +6,14 @@ import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 
 
 public class Manipulator extends SubsystemBase{
+  private final Manipulator manipController = new Manipulator();
 // product-specific voltage->pressure conversion, see product manual
 // in this case, 250(V/5)-25
 // the scale parameter in the AnalogPotentiometer constructor is scaled from 1 instead of 5,
@@ -19,16 +21,19 @@ public class Manipulator extends SubsystemBase{
 double scale = 250;
 double offset = -25;
 AnalogPotentiometer pressureTransducer = new AnalogPotentiometer(/* the AnalogIn port*/ 2, scale, offset);
-
+private final DoubleSolenoid m_DoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 7);
+  private static final int kSolenoidButton = 1;
+  private static final int kDoubleSolenoidForward = 2;
+  private static final int kDoubleSolenoidReverse = 3;
 // scaled values in psi units
 double psi = pressureTransducer.get();  
 Compressor phCompressor = new Compressor(30, PneumaticsModuleType.REVPH);
 
   double current = phCompressor.getPressure();
+
   //phCompressor.enableAnalog();
   //phCompressor.disable();
   
-  Solenoid exampleSolenoidPH = new Solenoid(PneumaticsModuleType.REVPH, 1);
   
   //exampleSolenoidPH.set(true);
   //exampleSolenoidPH.set(false);
@@ -36,7 +41,7 @@ Compressor phCompressor = new Compressor(30, PneumaticsModuleType.REVPH);
 
   double hellotest = phCompressor.getPressure();
 
-  boolean enabled = phCompressor.enabled();
+  //boolean enabled = phCompressor.enabled();
   boolean pressureSwitch = phCompressor.getPressureSwitchValue();
   
   @Override
