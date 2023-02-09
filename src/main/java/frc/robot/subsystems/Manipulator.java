a8dfdf27d3dfc75ce9beeb6020ce6077908ad6b8
BLlakers <jdonnelly@boyslatinmd.com>
2023-02-09 16:39:00 -0500

    Manip test

--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -1,8 +1,5 @@
-
 package frc.robot.subsystems;
-
 import frc.robot.Constants;
-
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,11 +11,7 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-
-
-
-
-
+import edu.wpi.first.wpilibj.XboxController.Button;
 
 public class Manipulator extends SubsystemBase{
   
@@ -29,40 +22,15 @@ public class Manipulator extends SubsystemBase{
 double scale = 250;
 double offset = -25;
 AnalogPotentiometer pressureTransducer = new AnalogPotentiometer(/* the AnalogIn port*/ 2, scale, offset);
-//Mtprivate final DoubleSolenoid m_DoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 7);
 
 DoubleSolenoid exampleDoublePH = new DoubleSolenoid(30, PneumaticsModuleType.REVPH, 0, 7);
-
 
 //exampleDoublePH.set(kOff);
 //exampleDoublePH.set(kForward);
 
-//exampleDoublePCM.set(manipController.manipButtonA.kForward);
-//exampleDoublePCM.
-//DoublePCM.set(manipController.manipButtonB.kReverse);
-
-//private static final int kSolenoidButton = 1;
-//private static final int kDoubleSolenoidForward = 2;
-//private static final int kDoubleSolenoidReverse = 3;
-
-
 // scaled values in psi units
 double psi = pressureTransducer.get();  
 Compressor phCompressor = new Compressor(Constants.PHChannel, PneumaticsModuleType.REVPH);
-
-  double current = phCompressor.getPressure();
-
-  //phCompressor.enableAnalog();
-  //phCompressor.disable();
-  
-  
-  //exampleSolenoidPH.set(true);
-  //exampleSolenoidPH.set(false);
-  
-
-  double hellotest = phCompressor.getPressure();
-
-  //boolean enabled = phCompressor.enabled();
   boolean pressureSwitch = phCompressor.getPressureSwitchValue();
   @Override
   public void periodic() {
@@ -71,11 +39,6 @@ Compressor phCompressor = new Compressor(Constants.PHChannel, PneumaticsModuleTy
   //System.out.println(hellotest);
   
  }
-  
-//public void toggle(){
-  //exampleDoublePH.set(Value.kForward);
-  // exampleDoublePH.set(Value.kReverse);
-//}
 
 public CommandBase toggleGripper() {
   // Inline construction of command goes here.
@@ -86,26 +49,10 @@ public CommandBase toggleGripper() {
         /* one-time action goes here */
         //WP  - Add code here to toggle the gripper solenoid
         System.out.println("Toggled gripper solenoid");
-
         System.out.println(exampleDoublePH.get());
-        //exampleDoublePH.toggle(exampleDoublePH.set(Value.kForward.kReverse));
-        
-        if (exampleDoublePH.get() == Constants.kForward ) {
-          exampleDoublePH.set(Value.kReverse);
-        } 
-        else {
-          exampleDoublePH.set(Value.kForward);
-
-        }
-      
-        
-
-      
-        //exampleDoublePH.toggle();
-        //exampleDoublePH.toggle();
+        exampleDoublePH.set(Value.kReverse);
        }
       );
   }
 
 }
-
