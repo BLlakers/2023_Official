651e2f4994d12ce656d9f2dc383b0f604ce40c21
BLlakers <jdonnelly@boyslatinmd.com>
2023-02-14 17:00:52 -0500

    Accedental merge branch fixed

---
+++ src/main/java/frc/robot/commands/RotateArmCommand.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+import com.revrobotics.CANSparkMax;
+import java.util.function.DoubleSupplier;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.DriveTrain;
+
+public class RotateArmCommand extends CommandBase {
+  DoubleSupplier m_leftY;
+  Arm m_Arm; 
+  public RotateArmCommand(DoubleSupplier _leftY, Arm _Arm) {
+    m_leftY = _leftY;
+    m_Arm = _Arm;
+    addRequirements(m_Arm);
+  } 
+  @Override
+
+  public void initialize() {
+        m_Arm.armRotationMtr.setIdleMode(CANSparkMax.IdleMode.kBrake);
+      }
+      @Override
+    
+    public void execute () {
+        System.out.println(m_leftY.getAsDouble());
+    }  
+    @Override
+    public void end(boolean interrupted) {
+    }
+    public boolean isFinished() {
+      return false;
+    }
+
+}
