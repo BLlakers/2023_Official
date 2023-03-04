efed2311ab09931ef58cedef582e15ed1a9a2e6a
BLlakers <jdonnelly@boyslatinmd.com>
2023-03-04 13:47:20 -0500

    Basic code for ultrasonic sensor

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -4,12 +4,17 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.AnalogInput;
+
 public class Claw extends SubsystemBase{
 
 DoubleSolenoid exampleDoublePH = new DoubleSolenoid(30, PneumaticsModuleType.REVPH, 1, 8); //NO ROBOT CODE THIS IS SOLENOID PORT
+AnalogInput ultrasonicSensor = new AnalogInput(5);
 
 @Override
 public void periodic() {
+  SmartDashboard.putNumber("Ultrasonic Votlage", ultrasonicSensor.getVoltage());
   
  }
 
