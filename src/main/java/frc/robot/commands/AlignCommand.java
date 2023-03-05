b3fda76804ff833ad5aa59856641421d56e8d721
BLlakers <jdonnelly@boyslatinmd.com>
2023-03-05 11:29:49 -0500

    possibly working alignment code

--- src/main/java/frc/robot/commands/AlignCommand.java
+++ src/main/java/frc/robot/commands/AlignCommand.java
@@ -23,7 +23,7 @@ public class AlignCommand extends CommandBase {
 
   @Override
   public void execute() { // Runs multiple times
-
+    Double move = 0.0;
     // turn2 must somehow become a doubble supplier and the drivetrain is off too
     // finding the turning amount needed
     Double turn1 = 0.035; // how much the motor must turn to turn one degree, this number IS WRONG: it
@@ -35,14 +35,32 @@ public class AlignCommand extends CommandBase {
     // movement for the left(left joystick thing) y on the "joystick"
     DoubleSupplier lefty;
     lefty = () -> 0.0;
-
     // movement fo the left(left joystick thing) x on the "joystick"
     DoubleSupplier leftx;
     leftx = () -> 0.0;
 
     // finaly driving
-    m_DriveTrain.drive(() -> 0, () -> 0, () -> 0);
-    m_DriveTrain.drive(lefty, leftx, () -> turn2);
+
+    if (m_angle.getAsDouble() >= 13){
+      //too far to right so it moves to the left
+      move = -0.2;
+    }
+    else if (m_angle.getAsDouble() <= 5){
+      //too far to the left so it moves to the right
+      move = 0.2;
+    }
+
+    //actully driving
+    Double move1 = move;
+    //added to fix error in the lat statemnt
+
+    if (m_angle.getAsDouble() == 9.6){
+      m_DriveTrain.drive(() -> 0, () -> 0, () -> 0);
+    }
+    else{
+      m_DriveTrain.drive(lefty, leftx, () -> move1);
+    }
+    
     // make this so it ONLY runs once
 
   }
